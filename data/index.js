82a469561b8e53f0366b68494a82ac6d60fe98e9
KevinValladares06 <149841068+KevinValladares06@users.noreply.github.com>
2025-04-11 18:28:58 -0600

    Variables de colores inicio, contacto, agendar

--- data/index.js
+++ data/index.js
@@ -1,15 +1,13 @@
 import { indexPage } from "./pages/indexPage";
 import { contactoPage } from "./pages/contactoPage";
-//import { noticiasPage } from "./pages/noticiasPage";
-//import { galeriaPage } from "./pages/galeriaPage";
+
 
 
 const commonData = {
     navigation: [
         { url: '/index.html', label: 'Inicio' },
         { url: '/Contacto.html', label: 'Contacto' },
-        //{ url: '/noticias.html', label: 'Noticias' },
-        //{ url: '/contactus.html', label: 'Acerca de FLL' },
+       
     ]
 }
 
@@ -22,12 +20,6 @@ export const getPageContext = (pagePath) => {
         case '/Contacto.html':
             pageData = contactoPage
             break;
-        /*case '/noticias.html':
-            pageData = noticiasPage
-            break;
-        case '/galeria.html':
-            pageData = galeriaPage
-            break;*/
         default:
             break;
     }
